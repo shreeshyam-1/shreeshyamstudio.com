7c1e7e95e066c968f53b4f841fb75d6f406271ea
Abhishek Singhal <abhisinghal112001@gmail.com>
2024-05-04 18:30:31 +0530

    items title

--- Data/Card.js
+++ Data/Card.js
@@ -1,90 +1,90 @@
 const items = [
     {
-        heading:"",
+        heading:"PMI Vignet: Brass Long Body Bathroom taps and Kitchen taps with Foam Flow Chrome Finish and Quarter Turn Fitting ",
         image:"pmiVignet.jpg",
         price:"",
         discount:"",
         buttonLink:"https://maps.app.goo.gl/Ks15KiUBGbQxVuJE6"
     },
     {
-        heading:"",
+        heading:"Alpine Aura: Brass Long Body Bathroom taps and Kitchen taps with Foam Flow Chrome Finish and Quarter Turn Fitting ",
         image:"alpineAura.jpg",
         price:"",
         discount:"",
         buttonLink:"https://maps.app.goo.gl/Ks15KiUBGbQxVuJE6"
     },
     {
-        heading:"",
+        heading:"Alpine Corsa: Brass Long Body Bathroom taps and Kitchen taps with Foam Flow Chrome Finish and Quarter Turn Fitting ",
         image:"alpineCorsa.jpg",
         price:"",
         discount:"",
         buttonLink:"https://maps.app.goo.gl/Ks15KiUBGbQxVuJE6"
     },
     {
-        heading:"",
+        heading:"Alpine Siya: Brass Long Body Bathroom taps and Kitchen taps with Foam Flow Chrome Finish and Quarter Turn Fitting ",
         image:"alpineSiya.jpg",
         price:"",
         discount:"",
         buttonLink:"https://maps.app.goo.gl/Ks15KiUBGbQxVuJE6"
     },
     {
-        heading:"",
+        heading:"Alpine Fusion: Brass Long Body Bathroom taps and Kitchen taps with Foam Flow Chrome Finish and Quarter Turn Fitting ",
         image:"alpineFusion.jpg",
         price:"",
         discount:"",
         buttonLink:"https://maps.app.goo.gl/Ks15KiUBGbQxVuJE6"
     },
     {
-        heading:"",
+        heading:"RSY Black: Black Finish Brass Long Body Bathroom taps and Kitchen taps with Foam Flow and Quarter Turn Fitting",
         image:"black.jpg",
         price:"",
         discount:"",
         buttonLink:"https://maps.app.goo.gl/Ks15KiUBGbQxVuJE6"
     },
     {
-        heading:"",
+        heading:"RSY Rose Gold: Rose Gold Finish Brass Long Body Bathroom taps and Kitchen taps with Foam Flow and Quarter Turn Fitting",
         image:"gold.jpg",
         price:"",
         discount:"",
         buttonLink:"https://maps.app.goo.gl/Ks15KiUBGbQxVuJE6"
     },
     {
-        heading:"",
+        heading:"CERA Chromo: Brass Long Body Bathroom taps and Kitchen taps with Foam Flow Chrome Finish and Quarter Turn Fitting",
         image:"ceraC.jpg",
-        price:"",
-        discount:"",
+        price:"25",
+        discount:"1",
         buttonLink:"https://maps.app.goo.gl/Ks15KiUBGbQxVuJE6"
     },
     {
-        heading:"",
+        heading:"CERA Victor: Brass Long Body Bathroom taps and Kitchen taps with Foam Flow Chrome Finish and Quarter Turn Fitting ",
         image:"ceraV.jpg",
         price:"",
         discount:"",
         buttonLink:"https://maps.app.goo.gl/Ks15KiUBGbQxVuJE6"
     },
     {
-        heading:"",
+        heading:"Top Spin A-1: Brass Long Body Bathroom taps and Kitchen taps with Foam Flow Chrome Finish and Quarter Turn Fitting ",
         image:"topspinA1.jpg",
         price:"",
         discount:"",
         buttonLink:"https://maps.app.goo.gl/Ks15KiUBGbQxVuJE6"
     },
     {
-        heading:"",
+        heading:"Top Spin J-Star: Brass Long Body Bathroom taps and Kitchen taps with Foam Flow Chrome Finish and Quarter Turn Fitting ",
         image:"topspinJstar.jpg",
         price:"",
         discount:"",
         buttonLink:"https://maps.app.goo.gl/Ks15KiUBGbQxVuJE6"
     },
     {
-        heading:"",
+        heading:"Top Spin Small Stone: Brass Long Body Bathroom taps and Kitchen taps with Foam Flow Chrome Finish and Quarter Turn Fitting ",
         image:"topspinSmallStone.png",
         price:"",
         discount:"",
         buttonLink:"https://maps.app.goo.gl/Ks15KiUBGbQxVuJE6"
     },
     {
-        heading:"",
+        heading:"Top Spin Touch: Brass Long Body Bathroom taps and Kitchen taps with Foam Flow Chrome Finish and Quarter Turn Fitting ",
         image:"topspinTouch.jpg",
         price:"",
         discount:"",
--- components/Card.js
+++ components/Card.js
@@ -5,21 +5,19 @@ const Card = ({data,key}) => {
     return (
 
 
-        <div key={key} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <Image height={100} width={300} className="p-8 rounded-t-lg w-full" src={`/images/items/tap/${data.image}`} alt="product image" />  
+        <div key={key} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-between">
+            <Image height={100} width={300} className="p-8 rounded-t-lg w-full" src={`/images/items/tap/${data.image}`} alt={data.heading} />  
             <div className="px-5 pb-5">
-                <a href="#">
-                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-                </a>
-                <div className='flex text-black my-2 gap-2 items-center'>
+                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{data.heading}</h5>
+                {data.price && (<div className='flex text-black my-2 gap-2 items-center'>
                     <div className='  font-bold text-2xl '>
-                        ₹500
+                        ₹{data.price}
                     </div>
-                    <span className='text-md font-semibold text-blue-500'>
-                        (up to 50% discount in-store )
-                    </span>
-                </div>
-                <div className="flex items-center gap-5">
+                    {data.discount && <span className='text-xl font-semibold text-red-600'>
+                        (up to {data.discount}% discount in-store )
+                    </span>}
+                </div>)}
+                <div className="flex items-center gap-5 mt-3">
                     {/* <span className="text-3xl font-bold text-gray-900 dark:text-white">₹99</span> */}
                     <a href="https://maps.app.goo.gl/Ks15KiUBGbQxVuJE6" target='_blank' className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buy Now</a>
                     {/* <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a> */}
