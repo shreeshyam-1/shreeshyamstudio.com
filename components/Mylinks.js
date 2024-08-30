ec7798283c3045c43a8f27d0042ddea588d9d41c
Abhishek Singhal <abhisinghal112001@gmail.com>
2024-08-30 13:47:47 +0530

    tank data

--- components/Mylinks.js
+++ components/Mylinks.js
@@ -9,7 +9,7 @@ export const links = [
           { name: "Taps", link: "/categories_faucet_taps" },
           { name: "Swan Neck", link: "/categories_faucet_swanNeck" },
           { name: "Wall Mixture", link: "/categories_faucet_wallMixture" },
-          { name: "Swan Neck", link: "/categories_faucet_swanNeck" },
+          { name: "Sink Cock", link: "/categories_faucet_sinkCock" },
         ],
       },
       {
@@ -18,6 +18,7 @@ export const links = [
           { name: "Hanging", link: "/categories_sanitaryware_hanging" },
           { name: "Pedestal", link: "/categories_sanitaryware_pedestal" },
           { name: "One Piece Washbasin", link: "/categories_sanitaryware_onePieceWashbasin" },
+          { name: "Table Top", link: "/categories_sanitaryware_tableTop" },
           { name: "Vanities", link: "/categories_sanitaryware_vanities" },
           { name: "EWC", link: "/categories_sanitaryware_ewc" },
         ],
@@ -28,6 +29,20 @@ export const links = [
           { name: "Bathroom", link: "/categories_accessories_bathroom" },
           { name: "Faucet", link: "/categories_accessories_faucet" },
 
+        ],
+      },
+      {
+        Head: "Water Tank",
+        sublink: [
+          { name: "Tank", link: "/categories_tank_tank" },
+        ],
+      },
+      {
+        Head: "Mirrors",
+        sublink: [
+          { name: "Mirror", link: "/categories_mirrors_mirror" },
+          { name: "Plain Mirror", link: "/categories_mirrors_plainMirror" },
+          { name: "Light Mirror", link: "/categories_mirrors_LightMirror" },
         ],
       },
     ]
