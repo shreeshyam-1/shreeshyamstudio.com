29cf8824b9891cb56786bc1729f263d3b1d5b3de
shreeshyam-1 <shreeshyamstudio2024@gmail.com>
2025-01-14 21:30:09 +0530

    Update gown.js

--- Data/gown.js
+++ Data/gown.js
@@ -15,6 +15,16 @@
 
 const gownData = [ 
  {
+        heading:"SG-63",
+        image:"Photoroom-20250114_190538[1].png",
+        categories:"Gown",
+        company:"",
+        price:"",
+        discount:"",
+        buttonLink:"",
+        path:"gown/"
+    },
+ {
         heading:"SG-67",
         image:"Photoroom-20250114_190337[1].png",
         categories:"Gown",
