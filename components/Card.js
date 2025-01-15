45042c270b92d40b0945c02baadb6b6e0acfb866
Abhishek Singhal <86764280+Abhimrt@users.noreply.github.com>
2025-01-15 10:51:37 +0530

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -36,7 +36,7 @@ const Card = ({ data, Key }) => {
         <div className="flex items-center gap-5 mt-3">
           {/* <span className="text-3xl font-bold text-gray-900 dark:text-white">₹99</span> */}
           <a
-            href="https://maps.app.goo.gl/Ks15KiUBGbQxVuJE6"
+            href="https://wa.me/+918802035717"
             target="_blank"
             className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
