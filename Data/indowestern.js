0dcc51b8c1bb99477ab0708c78af29814b5ddd32
shreeshyam-1 <shreeshyamstudio2024@gmail.com>
2025-01-19 20:44:02 +0530

    Update indowestern.js

--- Data/indowestern.js
+++ Data/indowestern.js
@@ -14,7 +14,16 @@
  */
 
 const indoData = [
-    
+    {
+        heading:"Indo-06",
+        image:"Photoroom-20250116_143513[1].png",
+        categories:"Indo Western",
+        company:"",
+        price:"",
+        discount:"",
+        buttonLink:"",
+        path:"indowestern/"
+    },
 
 
 ]
